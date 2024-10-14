d81e55f55065f93ecd6c12b12cf8ac5fac2b696d
Farwa-Kanwal-123 <wahabzahid06@gmail.com>
2024-10-14 05:11:15 +0500

    1st-next.js-website

---
+++ app/routes/contact/page.tsx
@@ -0,0 +1,52 @@
+import Link from "next/link";
+import React from "react";
+export default function Contact() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center border-teal-950 ">
+      <div className=" bg-green-200 text-white p-6 rounded-lg w-[50vw]">
+        <form className=" flex flex-col space-y-4 space-x-2 ">
+          <h1 className="font-bold bg-green-400 p-4 text-5xl text-center rounded-md">
+            Contact Form
+          </h1>
+
+          <input type="text" className="rounded-md p-2" placeholder="Name.." />
+
+          <input
+            type="email"
+            className="rounded-md  p-2"
+            placeholder="Email.."
+          />
+
+          <textarea
+            className="rounded-md p-2 resize-none"
+            placeholder="comment.."
+          ></textarea>
+
+          <button
+            type="submit"
+            className="bg-green-400 p-2 rounded-md text-white text-lg font-semibold cursor-pointer hover:bg-green-500 hover:font-semibold transition-all duration-100 "
+          >
+            Submit
+          </button>
+
+          <div className="flex justify-center gap-4 mt-3">
+            <Link
+              href={"/"}
+              className="bg-green-400 rounded-md p-1 hover:bg-white hover:text-green-500 
+              hover:font-bold transition-all duration-100"
+            >
+              Home
+            </Link>
+            <Link
+              href={"/about"}
+              className="bg-green-400 rounded-md p-1  hover:bg-white hover:text-green-500 
+              hover:font-bold transition-all duration-100"
+            >
+              About
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
